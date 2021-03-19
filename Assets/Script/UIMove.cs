c59920908d011324cb85f2b89f3719e68b01a2ee
uria <uria1106@gmail.com>
2021-03-19 17:12:48 +0900

    UIの丸く塗りつぶすやつ

--- Assets/Script/UIMove.cs
+++ Assets/Script/UIMove.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;//DOTweenを使用するため
+using UnityEngine.UI;
 
 public class UIMove : MonoBehaviour
 {
-    RectTransform target;
+    //RectTransform target;
+    Image target;
     // Start is called before the first frame update
     void Start()
     {
-        target = gameObject.GetComponent<RectTransform>();
-        target.DOAnchorPos(new Vector2(240f,0),0.6f).SetEase(Ease.OutBack);//移動
-        target.DOLocalRotate(new Vector3(360f,0,0),0.6f,RotateMode.FastBeyond360).SetEase(Ease.OutCubic);//回転
-        target.localScale = Vector3.one * 0.2f;
-        target.DOScale(1f,0.6f).SetEase(Ease.OutBack,5f);//拡大
+        //target = gameObject.GetComponent<RectTransform>();
+        //target.DOAnchorPos(new Vector2(240f,0),0.6f).SetEase(Ease.OutBack);//移動
+        //target.DOLocalRotate(new Vector3(360f,0,0),0.6f,RotateMode.FastBeyond360).SetEase(Ease.OutCubic);//回転
+        //target.localScale = Vector3.one * 0.2f;
+        //target.DOScale(1f,0.6f).SetEase(Ease.OutBack,5f);//拡大
+        target = gameObject.GetComponent<Image>();
+        //target.DOColor(new Color(1f, 0, 0), 1.5f);
+        //target.DOFade(0.2f,1.5f);
+        target.DOFillAmount(1f,2f).SetEase(Ease.InQuart);
 
     }
 
