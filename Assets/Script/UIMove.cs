213a015a97655d1a828e0d0fe27b3b861c016949
uria <uria1106@gmail.com>
2021-03-19 16:57:50 +0900

    UIに対してのDOTween

---
+++ Assets/Script/UIMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;//DOTweenを使用するため
+
+public class UIMove : MonoBehaviour
+{
+    RectTransform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = gameObject.GetComponent<RectTransform>();
+        target.DOAnchorPos(new Vector2(240f,0),0.6f).SetEase(Ease.OutBack);//移動
+        target.DOLocalRotate(new Vector3(360f,0,0),0.6f,RotateMode.FastBeyond360).SetEase(Ease.OutCubic);//回転
+        target.localScale = Vector3.one * 0.2f;
+        target.DOScale(1f,0.6f).SetEase(Ease.OutBack,5f);//拡大
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
