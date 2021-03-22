84e0bd09f866ebe6b6096c28c532c5435ff07772
uria <uria1106@gmail.com>
2021-03-22 22:44:50 +0900

    テキストの一部をトゥイーンする方法 これはゲーム内で利用できると面白いかも

--- Assets/Script/TextMove.cs
+++ Assets/Script/TextMove.cs
@@ -20,7 +20,18 @@ public class TextMove : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space)){
             //TargetText.DOText("うりあの魔導書",1f, scrambleMode :ScrambleMode.All);
-            TargetText.DOCounter(1,1000,1.5f,true);//trueにすると3桁区切りでカンマを挿入する
+            //TargetText.DOCounter(1,1000,1.5f,true);//trueにすると3桁区切りでカンマを挿入する
+
+            DOTween.To(
+                //開始の値
+                () => 0,
+                //更新処理
+                x => TargetText.text = $"HP : {x:N0}",
+                //完了の値
+                1000,
+                //トゥイーンの時間(秒)
+                1.5f
+            );//通常版の文法を使用することで文章内の一部をトゥイーンさせることが可能
         }
     }
 }
