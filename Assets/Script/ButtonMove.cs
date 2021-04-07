52c7501f922eb67bc115b7f034197d66a5496ef2
uria <uria1106@gmail.com>
2021-04-07 22:30:04 +0900

    ボタンのアニメーションの調査

---
+++ Assets/Script/ButtonMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ButtonMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnClick()
+    {
+        //DOTween.Sequence().Append(transform.DOScale(1.3f,0.5f).SetEase(Ease.OutElastic))
+        //.Append(transform.DOScale(0.8f,0.5f).SetEase(Ease.OutElastic));
+        //transform.DOScale(1.2f,0.5f).SetEase(Ease.OutElastic).SetLoops(-1,LoopType.Yoyo);
+        transform.DOScale(1.2f,0.5f).SetEase(Ease.OutElastic).SetLoops(-1,LoopType.Restart);
+    }
+}
