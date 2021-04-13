7fecc814f7d1d3d264ad5361f6be7c26d6224a33
uria <uria1106@gmail.com>
2021-04-13 21:52:09 +0900

    α値を利用して見えなくしている

--- Assets/Script/Shot.cs
+++ Assets/Script/Shot.cs
@@ -9,6 +9,17 @@ public class Shot : MonoBehaviour
     void Start()
     {
         transform.DOLocalMove(new Vector3(10f,0,0),1f).SetRelative(true).OnComplete(Finish);//相対位置で移動する。完了した場合Finish()を実行する
+
+        var SpriteImage = GetComponent<SpriteRenderer>();
+        var SpriteColor = SpriteImage.color;
+        SpriteColor.a = 1.0f;
+        SpriteImage.color = SpriteColor;
+        DOTween.ToAlpha(
+            ()=> SpriteImage.color,
+            color => SpriteImage.color = color,
+            0f,
+            1f
+        );
     }
 
     // Update is called once per frame
