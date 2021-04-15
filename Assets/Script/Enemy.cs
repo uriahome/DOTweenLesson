0e2fef5e71f836fae88d15a59fa4c8c4896895c5
uria <uria1106@gmail.com>
2021-04-15 22:45:05 +0900

    特定の地点を経由して動き回る

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;//DOTweenを使用するため
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Move();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Move(){
+        Debug.Log("Move!!");
+        DOTween.Sequence().Append(transform.DOLocalMove(new Vector3(5,5,0),1f))
+        .Append(transform.DOLocalMove(new Vector3(8,0,0),1f))
+        .Append(transform.DOLocalMove(new Vector3(5,-5,0),1f))
+        .OnComplete(Move);
+    }
+}
