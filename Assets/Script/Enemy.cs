70b70ddd797350f6bd0e0703581304a175f6a68a
uria <uria1106@gmail.com>
2021-04-16 19:04:56 +0900

    行動の変化を入れてみた 切り替わりのタイミングが止まるので 微妙

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -6,8 +6,11 @@ using DG.Tweening;//DOTweenを使用するため
 public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] bool ChangeMoveFlag;
     void Start()
     {
+        ChangeMoveFlag = false;
+        StartCoroutine("ChangeMove");
         Move();
     }
 
@@ -19,9 +22,22 @@ public class Enemy : MonoBehaviour
 
     void Move(){
         Debug.Log("Move!!");
-        DOTween.Sequence().Append(transform.DOLocalMove(new Vector3(5,5,0),1f))
-        .Append(transform.DOLocalMove(new Vector3(8,0,0),1f))
-        .Append(transform.DOLocalMove(new Vector3(5,-5,0),1f))
-        .OnComplete(Move);
+        if(ChangeMoveFlag){
+            DOTween.Sequence().Append(transform.DOLocalMove(new Vector3(5,-5,0),1f))
+            .Append(transform.DOLocalMove(new Vector3(8,0,0),1f))
+            .Append(transform.DOLocalMove(new Vector3(5,5,0),1f))
+            .OnComplete(Move);
+        }else{
+            DOTween.Sequence().Append(transform.DOLocalMove(new Vector3(5,5,0),1f))
+            .Append(transform.DOLocalMove(new Vector3(8,0,0),1f))
+            .Append(transform.DOLocalMove(new Vector3(5,-5,0),1f))
+            .OnComplete(Move);
+        }
+    }
+
+    IEnumerator ChangeMove(){
+        yield return new WaitForSeconds(5f);
+        ChangeMoveFlag = !ChangeMoveFlag;
+        StartCoroutine("ChangeMove");
     }
 }
