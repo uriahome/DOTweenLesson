c01787688117d695e33cc36bd86e855fd23200ed
uria <uria1106@gmail.com>
2021-04-20 21:14:15 +0900

    Sequence()に.Joinで追加すると同時処理の実装ができる。

---
+++ Assets/Script/Spyder.cs
@@ -0,0 +1,23 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;//DOTweenを使用するため
+
+public class Spyder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DOTween.Sequence().Append(transform.DOLocalMoveX(5f,1f))
+        .Append(transform.DOLocalMoveY(2f,1f))
+        .Join(transform.DOScale(2.5f,0.3f))
+        .Append(transform.DOLocalMoveX(0f,1f))
+        .Join(transform.DOScale(1f,0.3f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
