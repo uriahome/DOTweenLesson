4e8c0f3c89761c08651749724c93a7c814861e38
uria <uria1106@gmail.com>
2021-04-22 20:27:54 +0900

    少しずつ移動する

--- Assets/Script/Spyder.cs
+++ Assets/Script/Spyder.cs
@@ -9,17 +9,24 @@ public class Spyder : MonoBehaviour
     void Start()
     {
 
-        var Seq = DOTween.Sequence();
+        /*var Seq = DOTween.Sequence();
         Seq.Append(transform.DOLocalMoveX(5f,1f));
         Seq.Append(transform.DOLocalMoveY(2f,1f));
         Seq.Join(transform.DOScale(2.5f,0.3f));
         Seq.Append(transform.DOLocalMoveX(0f,1f));
         Seq.Join(transform.DOScale(1f,0.3f));
+        */
+         
+        transform.DOLocalMove(new Vector3(this.transform.position.x+3f,0,0),2f).From(new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z)).OnComplete(Move);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Move(){
+        transform.DOLocalMove(new Vector3(this.transform.position.x+3f,0,0),2f).From(new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z)).OnComplete(Move);
     }
 }
