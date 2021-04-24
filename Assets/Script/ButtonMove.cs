0cadeb40fc7c5384f341e0e0649c57d61c13380f
uria <uria1106@gmail.com>
2021-04-24 22:59:16 +0900

    ボタンが目立つようにするために拡大縮小を繰り替えるようにした

--- Assets/Script/ButtonMove.cs
+++ Assets/Script/ButtonMove.cs
@@ -8,32 +8,40 @@ public class ButtonMove : MonoBehaviour
 {
     // Start is called before the first frame update
     public bool IsClick;
+    Button button;
     void Start()
     {
         IsClick = false;
-        Button button = GetComponent<Button>();
+        button = GetComponent<Button>();
         button.onClick.AddListener(OnClick);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnClick()
     {
         //DOTween.Sequence().Append(transform.DOScale(1.3f,0.5f).SetEase(Ease.OutElastic))
         //.Append(transform.DOScale(0.8f,0.5f).SetEase(Ease.OutElastic));
-        //transform.DOScale(1.2f,0.5f).SetEase(Ease.OutElastic).SetLoops(-1,LoopType.Yoyo);
+        transform.DOScale(1.2f,0.5f).SetEase(Ease.OutElastic).SetLoops(-1,LoopType.Yoyo);//拡大縮小を繰り返す
         //transform.DOScale(1.2f,0.5f).SetEase(Ease.OutElastic).SetLoops(-1,LoopType.Restart);
-        if(IsClick){
-            transform.DOScale(1.0f,0.5f).SetEase(Ease.OutElastic);//縮小
+
+
+        /*if (IsClick)
+        {
+            transform.DOScale(1.0f, 0.5f).SetEase(Ease.OutElastic);//縮小
             GetComponentInChildren<Text>().text = "小さい";
-        }else{
-            transform.DOScale(1.4f,0.5f).SetEase(Ease.OutElastic);//巨大化
+        }
+        else
+        {
+            transform.DOScale(1.4f, 0.5f).SetEase(Ease.OutElastic);//巨大化
             GetComponentInChildren<Text>().text = "大きい";
         }
         IsClick = !IsClick;//bool反転ですっきりさせた
+        */
     }
+
 }
