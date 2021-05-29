5ae0bcc58e1185bd69861ab689e3748baac1a8bd
uria <uria1106@gmail.com>
2021-05-29 11:55:04 +0900

    !マークの画像を使って強調のアニメーションを作成

---
+++ Assets/Script/ImageMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ImageMove : MonoBehaviour
+{
+    [SerializeField] GameObject Bikkuri;
+    [SerializeField] RectTransform RBikkuri;
+    [SerializeField] Image BikkuriImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RBikkuri = Bikkuri.GetComponent<RectTransform>();
+       /* DOTween.Sequence().Append(BikkuriImage.DOFade(0.1f, 0.95f))
+        .Join(Bikkuri.transform.DOScale(1.2f,1.0f))
+        .Join(RBikkuri.DOJumpAnchorPos(new Vector3(0,0,0),50f,1,1f))
+        .SetLoops(-1,LoopType.Restart);//拡大と透過とジャンプを繰り返す*/
+
+        DOTween.Sequence().Append(Bikkuri.transform.DOScale(1.2f,1.0f))
+        .Join(RBikkuri.DOJumpAnchorPos(new Vector3(0,0,0),50f,1,1f))
+        .SetLoops(-1,LoopType.Restart);//拡大とジャンプを繰り返す
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
