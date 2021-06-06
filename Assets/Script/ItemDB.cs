da958791543ad302cf8d4b90e015c04900c30f0b
uria <uria1106@gmail.com>
2021-06-06 12:12:45 +0900

    辞書型の勉強

---
+++ Assets/Script/ItemDB.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDB : MonoBehaviour
+{
+    public List<Item> ItemList = new List<Item>();
+
+    public Dictionary<int, Item> ItemDictionary  = new Dictionary<int,Item>();
+
+    private void Start()
+    {
+        Item sword = new Item();
+        sword.name ="Sword";
+        sword.id = 0;
+
+        ItemList.Add(sword);
+        ItemDictionary.Add(0,sword);
+
+        Item shield = new Item();
+        shield.name ="Shield";
+        shield.id = 1;
+        ItemDictionary.Add(1,shield);
+
+        Item Armor = new Item();
+        Armor.name = "Armor";
+        Armor.id = 2;
+        ItemDictionary.Add(2,Armor);
+
+        foreach(var key in ItemDictionary.Keys){
+            Debug.Log(key + ":" + ItemDictionary[key].id + ":" + ItemDictionary[key].name);
+        }
+
+    }
+
+}
